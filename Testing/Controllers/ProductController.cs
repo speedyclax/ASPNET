5e969db01409432ed77be5a7e073a850a9a31c79
Jon Claxton <speedyclax@gmail.com>
2024-01-14 16:08:56 -0600

    exercise completed

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+    }
+}
---
+++ Testing/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Testing.Models
+{
+    public class Product
+    {
+        public Product() 
+        { 
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string CategoryID { get; set; }
+        public string OnSale { get; set; }
+        public string StockLevel { get; set; }
+
+
+
+    }
+}
